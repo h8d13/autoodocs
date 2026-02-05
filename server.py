66eaea7108a857fcb26fba2793ace26ca934f8f9
h8d13 <hadrien.loge@gmail.com>
2026-02-06 00:07:55 +0100

    20260206_0007

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import http.server
-import sys
 
-PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
+PORT = 8080
 
 print(f'Serving on http://localhost:{PORT}')
 http.server.HTTPServer(('', PORT), http.server.SimpleHTTPRequestHandler).serve_forever()
