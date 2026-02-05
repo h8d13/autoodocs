14f92c5dd9fdb704a401bf79375d1bf8699682cd
h8d13 <hadrien.loge@gmail.com>
2026-02-06 00:09:09 +0100

    20260206_0009

--- server.py
+++ server.py
@@ -4,4 +4,8 @@ import http.server
 PORT = 8080
 
 print(f'Serving on http://localhost:{PORT}')
-http.server.HTTPServer(('', PORT), http.server.SimpleHTTPRequestHandler).serve_forever()
+
+try:
+    http.server.HTTPServer(('', PORT), http.server.SimpleHTTPRequestHandler).serve_forever()
+except KeyboardInterrupt:
+    raise SystemExit(0)
