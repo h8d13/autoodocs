f5e1867fa2916a35434cf45b30665e510f5e5f01
h8d13 <hadean-eon-dev@proton.me>
2026-02-06 14:22:54 +0100

    css

--- build.lua
+++ build.lua
@@ -2,6 +2,7 @@
 -- Build: generate docs and convert to HTML
 
 local fmt = string.format
+local stats = arg[1] == "-s"
 
 print("Generating markdown...")
 os.execute("lua autodocs.lua . docs")
@@ -15,5 +16,9 @@ for md in pipe:lines() do
     os.execute(fmt("lua markdown.lua %s", md))
 end
 pipe:close()
+
+if stats then
+    os.execute("awk -f stats.awk docs/*.md")
+end
 
 print("Done! Open docs/index.html")
